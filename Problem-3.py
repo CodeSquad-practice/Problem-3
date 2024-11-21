6fb60c4b758541742e83c43f6a44b6061c50c0c9
2jiyong <jdragon2000@naver.com>
2024-11-21 21:08:17 +0900

    Add main function

--- Problem-3.py
+++ Problem-3.py
@@ -1,5 +1,12 @@
 import random
+
 def get_random_card():
     card=random.randint(1,12)
     return card
-print(get_random_card())
+
+def main():
+    card=get_random_card()
+    print(card)
+
+if __name__=='__main__':
+    main()
