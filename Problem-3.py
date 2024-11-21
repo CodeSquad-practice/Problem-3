8f60c9159ccad6467395c10662ba36df30328a91
2jiyong <jdragon2000@naver.com>
2024-11-21 21:22:51 +0900

    Add function to print record and check who is winner

--- Problem-3.py
+++ Problem-3.py
@@ -4,12 +4,17 @@ def getRandomCard():
     card=random.randint(1,12)
     return card
 
-def 
+def printRecord(win,draw,lose):
+    if draw==0:
+        print(f'현재전적 : {win}승 {lose}패')
+    else:
+        print(f'현재전적 : {win}승 {draw}무 {lose}패')
 
 def main():
     turn=0
     win=0
     lose=0
+    draw=0
     print('간단 카드게임을 시작합니다.')
     print()
     while True:
@@ -19,6 +24,17 @@ def main():
         dealerCard=getRandomCard()
         print(f'YOU   : [{myCard:2}]')
         print(f'DEALER: [{dealerCard:2}]')
+
+        if myCard>dealerCard:
+            win+=1
+            print('당신이 이겼습니다.')
+        elif myCard==dealerCard:
+            draw+=1
+            print('비겼습니다.')
+        else:
+            lose+=1
+            print('딜러가 이겼습니다.')
+        printRecord(win,draw,lose)
         break
 
 if __name__=='__main__':
